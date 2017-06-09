805fbb626a0de75cd4d0074ae3df8825b385790a
hyeonho88 <hunho88@naver.com>
2017-06-09 14:19:03 +0900

    0609

--- Source/MNIST/minibatch.py
+++ Source/MNIST/minibatch.py
@@ -4,6 +4,14 @@ sys.path.append("c:/users/hunho/OneDrive/문서/GitHub/supersuper/firstgit/Machi
 import numpy as np
 from mnist import load_mnist
 
+def CEE(y,t):
+    if y.ndim == 1:
+        t = t.reshape(1,t.size)
+        y = y.reshape(1,y.size)
+
+    batch_size = y_shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
+
 (x_train,t_train),(x_test,t_test)=\
     load_mnist(normalize=True,one_hot_label=True)
 print(x_train.shape)
